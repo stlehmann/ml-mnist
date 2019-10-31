70523642b6415425c1352ca7ab98bf4f8ecd222a
Stefan Lehmann <stlm@posteo.de>
2019-10-31 10:40:55 +0100

    Add setup.py

---
+++ src/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name="mnist",
+    version="0.0.1",
+    packages=["mnist"],
+    install_requires=[],
+    entry_points={
+        "console_scripts": [
+        ]
+    }
+)
